2d5dea18229a1af6706463d28a8cd8e6dc3f9ed7
Nibicu <Wams9119@Gmail.com>
2024-12-13 20:23:32 +0700

    Add project files.

---
+++ Tasks1/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace Tasks1
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            var class1 = new Class1();
+            Console.Write("Введите ваше ФИО: ");
+            var firstName = Console.ReadLine();
+            Console.Write("Введите вашу дату рождения (дд.мм.гггг): ");
+            var dateOfBirth = Console.ReadLine();
+            Console.WriteLine($"\nВаши данные!\n ФИО - {firstName}\n Дата рождения - {dateOfBirth}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tasks2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Tasks2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();        // Хотел сделать задание "квест" потом вспомнил, что говорили про Random. В итоге получилась игра.
+            string[] maps = { "красный", "синий" };
+            int balls = 100;
+
+            Console.WriteLine("Добро пожаловать в игру 'Угадай Карту'\n        Правила!!!");
+            Console.WriteLine("У вас есть 100 баллов. Вы загадываете цвет: красный или синий. После чего мы поднимаем карту.\nУгадали: +100 баллов, если нет -100. Игра закончится если вы потеряете все баллы");
+
+            while (balls >= 0)
+            {
+                string randomCart = maps[random.Next(maps.Length)];
+
+                Console.Write("\nКарта на столе! Введите ваш ответ (красный/синий): ");
+                var userGuess = Console.ReadLine();
+
+                if (userGuess != "красный" && userGuess != "синий")
+                {
+                    Console.WriteLine("Ошибка, введите 'красный' или 'синий'.");
+                    continue;
+                }
+
+                if (userGuess == randomCart)
+                {
+                    balls += 100;
+                    Console.WriteLine($"Вы угадали! Цвет был {randomCart}. Ваши баллы: {balls}");
+                }
+                else
+                {
+                    balls -= 100;
+                    Console.WriteLine($"Вы не угадали. Цвет был {randomCart}. Ваши баллы: {balls}");
+                }
+
+                if (balls == 0)
+                {
+                    Console.WriteLine("Вы проиграли! У вас закончались баллы.");
+                    break;
+                }
+            }
+        }
+    }
+}
